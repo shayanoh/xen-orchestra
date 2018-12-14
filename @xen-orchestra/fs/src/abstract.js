3bed36a880db3aacd3a60897ad4856cae80fac54
Enishowk <kenshineuh@gmail.com>
2018-12-14 17:23:56 +0100

    getAllRemotesInfo method

--- @xen-orchestra/fs/src/abstract.js
+++ @xen-orchestra/fs/src/abstract.js
@@ -414,7 +414,7 @@ export default class RemoteHandlerAbstract {
   async _forget(): Promise<void> {}
 
   async _getInfo() {
-    return {}
+    return Promise.resolve({})
   }
 
   async _getSize(file: File): Promise<number> {
